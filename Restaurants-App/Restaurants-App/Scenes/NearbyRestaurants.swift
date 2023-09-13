9509c0d543f979da77414055e24181ed15d669f5
Douglas Amorim <amorimdos@gmail.com>
2023-09-13 15:43:07 -0300

    feat create nearbyRestaurants View

--- Restaurants-App/Restaurants-App/Scenes/NearbyRestaurants.swift
+++ Restaurants-App/Restaurants-App/Scenes/NearbyRestaurants.swift
@@ -9,23 +9,6 @@ import Foundation
 import GooglePlaces
 import SwiftUI
 
-class Person {
-    let name: String
-    init(name: String) {
-        self.name = name
-    }
-    
-    func GetNewPerson1() -> Self {
-        return Person(name: "EPR") as! Self
-    }
-    
-    func GetNewPerson2() -> Self {
-        // **Error** below: Cannot convert return expression of type 'Person' to return type 'Self'
-        return Person(name: "Bella")
-    }
-}
-
-let person = Person(name: "King")
 
 struct NearbyRestaurants: View {
     private var placesClient: GMSPlacesClient = GMSPlacesClient.shared()
